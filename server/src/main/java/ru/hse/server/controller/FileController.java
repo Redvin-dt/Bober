d31e644fa50717fc326675e1ccd035cab078ab92
Redvin <pavel.afonkin.04@mail.ru>
2024-02-27 04:11:26 +0300

    add file validation

--- server/src/main/java/ru/hse/server/controller/FileController.java
+++ server/src/main/java/ru/hse/server/controller/FileController.java
@@ -1,21 +1,37 @@
 package ru.hse.server.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ru.hse.server.exception.FileValidationException;
 import ru.hse.server.service.FileService;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/files")
 public class FileController {
 
-    private final FileService fileService = new FileService();
+    static final Logger logger = LoggerFactory.getLogger(FileController.class);
+    private final FileService fileService;
+
+    private FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
 
     @PostMapping
     public ResponseEntity<String> loadFile(@RequestParam MultipartFile file) {
         try {
-            return ResponseEntity.ok().body(fileService.save(file));
-        } catch (Exception e){
+            var response = ResponseEntity.ok().body(fileService.save(file));
+            logger.info("File {} successful loaded", file.getOriginalFilename());
+            return response;
+        } catch (IOException e) {
+            logger.error("Can not load file {}, error message: {}", file.getOriginalFilename(), e.getMessage());
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (FileValidationException e) {
+            logger.error("Invalid file {}, error message: {}", file.getOriginalFilename(), e.getMessage());
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
@@ -23,8 +39,11 @@ public class FileController {
     @GetMapping
     public ResponseEntity<byte[]> getFile(@RequestParam String fileName) {
         try {
-            return ResponseEntity.ok().body(fileService.getFile(fileName));
-        } catch (Exception e){
+            var response = ResponseEntity.ok().body(fileService.getFile(fileName));
+            logger.debug("File with fileName={} successful gets", fileName);
+            return response;
+        } catch (IOException e) {
+            logger.error("Can not get file with fileName={}, error message: {}", fileName, e.getMessage());
             return ResponseEntity.badRequest().body(e.getMessage().getBytes());
         }
     }
