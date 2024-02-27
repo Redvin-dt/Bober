ddf8882f32618da6e6ba12f0749bd4f2e5b6c364
Redvin <pavel.afonkin.04@mail.ru>
2024-02-28 02:03:17 +0300

    add test

--- server/src/main/java/ru/hse/server/repository/FileRepository.java
+++ server/src/main/java/ru/hse/server/repository/FileRepository.java
@@ -1,9 +1,11 @@
 package ru.hse.server.repository;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Repository
 public interface FileRepository {
     void save(MultipartFile file, String name) throws IOException;
 
