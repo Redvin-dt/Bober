6e25fbfc59cc8f8169fbec1038e0be7dadd1f137
romanpovol <roman.a.p@bk.ru>
2024-03-09 17:02:57 +0300

    ui upgrade

--- Client/app/src/main/java/ru/hse/client/SignInActivity.kt
+++ Client/app/src/main/java/ru/hse/client/SignInActivity.kt
@@ -1,13 +1,83 @@
 package ru.hse.client
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import okhttp3.OkHttpClient
+import kotlin.math.sign
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_in)
+
+        val okHttpClient = OkHttpClient()
+
+        var loginEditText: TextInputEditText = findViewById(R.id.login)
+        var loginLayout: TextInputLayout = findViewById(R.id.login_box)
+
+        val textWatcherForLogin: TextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(charSequence: CharSequence, start: Int, before: Int, count: Int
+            ) {
+                val newText = charSequence.toString()
+                if (!isValidLogin(newText)) {
+                    loginLayout.error = "Unacceptable symbols in login"
+                } else {
+                    // TODO: get login from db and check
+                    loginLayout.boxStrokeColor =
+                        ContextCompat.getColor(this@SignInActivity, R.color.green)
+                    loginLayout.error = null
+                }
+            }
+
+            override fun afterTextChanged(editable: Editable) {
+            }
+        }
+        loginEditText.addTextChangedListener(textWatcherForLogin)
+
+        var passwordEditText: TextInputEditText = findViewById(R.id.password)
+        var passwordLayout: TextInputLayout = findViewById(R.id.password_box)
+
+        val textWatcherForPassword: TextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(charSequence: CharSequence, start: Int, before: Int, count: Int) {
+                val passwordOriginal = charSequence.toString()
+                if (passwordOriginal.length <= 3) {
+                    passwordLayout.error = "Original password is too short"
+                } else {
+                    passwordLayout.boxStrokeColor =  ContextCompat.getColor(this@SignInActivity, R.color.green)
+                    passwordLayout.error = null
+                }
+            }
+
+            override fun afterTextChanged(editable: Editable) {
+            }
+        }
+        passwordEditText.addTextChangedListener(textWatcherForPassword)
+
+        var signInButton: Button = findViewById(R.id.sign_in_button)
+
+        signInButton.setOnClickListener{
+            
+        }
+
+        var dontHaveAnAccountButton: Button = findViewById(R.id.dont_have_an_account)
+        dontHaveAnAccountButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
