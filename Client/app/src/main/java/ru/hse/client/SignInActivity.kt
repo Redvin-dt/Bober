75e232067eab6925d129f1b888fafea7721ac1cd
romanpovol <roman.a.p@bk.ru>
2024-03-26 21:26:55 +0300

    add utils class

--- Client/app/src/main/java/ru/hse/client/SignInActivity.kt
+++ Client/app/src/main/java/ru/hse/client/SignInActivity.kt
@@ -1,24 +1,16 @@
 package ru.hse.client
 
-
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import okhttp3.*
-import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
-import okio.ByteString
-import ru.hse.server.proto.EntitiesProto
-import java.io.IOException
 
 
 class SignInActivity : AppCompatActivity() {
@@ -29,9 +21,9 @@ class SignInActivity : AppCompatActivity() {
 
         val okHttpClient = OkHttpClient()
 
-        var loginEditText: TextInputEditText = findViewById(R.id.login)
-        var loginLayout: TextInputLayout = findViewById(R.id.login_box)
-        loginEditText.setOnFocusChangeListener{ v, hasFocus ->
+        val loginEditText: TextInputEditText = findViewById(R.id.login)
+        val loginLayout: TextInputLayout = findViewById(R.id.login_box)
+        loginEditText.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
                 hideKeyboard(v)
             }
@@ -45,7 +37,7 @@ class SignInActivity : AppCompatActivity() {
                 charSequence: CharSequence, start: Int, before: Int, count: Int
             ) {
                 val newText = charSequence.toString()
-                if (!isValidLogin(newText)) {
+                if (isNotValidLogin(newText)) {
                     loginLayout.error = "Unacceptable symbols in login"
                 } else {
                     // TODO: get login from db and check
@@ -60,9 +52,9 @@ class SignInActivity : AppCompatActivity() {
         }
         loginEditText.addTextChangedListener(textWatcherForLogin)
 
-        var passwordEditText: TextInputEditText = findViewById(R.id.password)
-        var passwordLayout: TextInputLayout = findViewById(R.id.password_box)
-        passwordEditText.setOnFocusChangeListener{ v, hasFocus ->
+        val passwordEditText: TextInputEditText = findViewById(R.id.password)
+        val passwordLayout: TextInputLayout = findViewById(R.id.password_box)
+        passwordEditText.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
                 hideKeyboard(v)
             }
@@ -74,7 +66,7 @@ class SignInActivity : AppCompatActivity() {
 
             override fun onTextChanged(charSequence: CharSequence, start: Int, before: Int, count: Int) {
                 val passwordOriginal = charSequence.toString()
-                if (!isValidPassword(passwordOriginal)) {
+                if (isNotValidPassword(passwordOriginal)) {
                     passwordLayout.error = "Incorrect password"
                 } else {
                     passwordLayout.boxStrokeColor = ContextCompat.getColor(this@SignInActivity, R.color.green)
@@ -87,126 +79,35 @@ class SignInActivity : AppCompatActivity() {
         }
         passwordEditText.addTextChangedListener(textWatcherForPassword)
 
-        var signInButton: Button = findViewById(R.id.sign_in_button)
+        val signInButton: Button = findViewById(R.id.sign_in_button)
 
         signInButton.setOnClickListener {
             val login: String = loginEditText.text.toString()
             val password: String = passwordEditText.text.toString()
 
-            if (!isValidLogin(login)) {
+            if (isNotValidLogin(login)) {
                 writeErrorAboutLogin(login, this)
                 Log.e("Sign in", "incorrect login: $login")
                 return@setOnClickListener
             }
 
-            if (!isValidPassword(password)) {
+            if (isNotValidPassword(password)) {
                 writeErrorAboutPassword(password, this)
                 Log.e("Sign in", "incorrect password: $password")
                 return@setOnClickListener
             }
 
-            val URlGetUser: String = ("http://" + getString(R.string.IP) + "/users/userByLogin").toHttpUrlOrNull()
-                ?.newBuilder()
-                ?.addQueryParameter("login", login)
-                ?.build().toString()
-
-            val requestForGetUser: Request = Request.Builder()
-                .url(URlGetUser)
-                .build()
-
-            okHttpClient.newCall(requestForGetUser).enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    Log.e("Error", e.toString() + " " + e.message)
-                    Handler(Looper.getMainLooper()).post {
-                        Toast.makeText(
-                            this@SignInActivity,
-                            "Something went wrong, try again",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-
-                var badUserMessage: String = "incorrect login/password"
-
-                fun printErrorAboutBadUser() {
-                    Log.e("Info", "no such user with this login")
-                    Handler(Looper.getMainLooper()).post {
-                        Toast.makeText(
-                            this@SignInActivity,
-                            badUserMessage,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        loginLayout.error = badUserMessage
-                        passwordLayout.error = badUserMessage
-                    }
-                }
-
-                fun printOkAboutGoodUser() {
-                    Log.e("Info", "user exist")
-                    Handler(Looper.getMainLooper()).post {
-                        Toast.makeText(
-                            this@SignInActivity,
-                            "Welcome",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-
-                fun printMessageFromBadResponse(message: CharSequence?) {
-                    if (message == null) {
-                        Log.e("Error", "null response message")
-                        Handler(Looper.getMainLooper()).post {
-                            Toast.makeText(
-                                this@SignInActivity,
-                                "Something went wrong, try again",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                        return
-                    }
-                    Log.e("Info", message.toString())
-                    Handler(Looper.getMainLooper()).post {
-                        Toast.makeText(
-                            this@SignInActivity,
-                            message,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    Log.i("Info", response.toString())
-                    if (response.isSuccessful) {
-                        val responseBody: ByteString? = response.body?.byteString()
-                        val user: EntitiesProto.UserModel =
-                            EntitiesProto.UserModel.parseFrom(responseBody?.toByteArray())
-                        if (user.hasPasswordHash()) {
-                            if (user.passwordHash == password) {
-                                printOkAboutGoodUser()
-                                // TODO: create main Activity
-                            } else {
-                                Log.e("Info", "no such login/password")
-                                printErrorAboutBadUser()
-                            }
-                        } else {
-                            Log.e("Info", "no such login/password")
-                            printErrorAboutBadUser()
-                        }
-                    } else {
-                        printMessageFromBadResponse(response.body?.string())
-                    }
-                }
-            })
+            tryToLogInUser(login, password, this@SignInActivity, okHttpClient, loginLayout, passwordLayout)
 
         }
 
-        var dontHaveAnAccountButton: Button = findViewById(R.id.dont_have_an_account)
+        val dontHaveAnAccountButton: Button = findViewById(R.id.dont_have_an_account)
         dontHaveAnAccountButton.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
-        var forgotPasswordButton: Button = findViewById(R.id.forgot_password)
+        val forgotPasswordButton: Button = findViewById(R.id.forgot_password)
         forgotPasswordButton.setOnClickListener {
             // TODO: create an opportunity to restore data
         }
--- Client/app/src/main/java/ru/hse/client/SignUpActivity.kt
+++ Client/app/src/main/java/ru/hse/client/SignUpActivity.kt
@@ -1,142 +1,17 @@
 package ru.hse.client
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.util.Patterns
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import okhttp3.*
-import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okio.ByteString
-import ru.hse.server.proto.EntitiesProto.UserModel
-import java.io.IOException
-import java.util.regex.Matcher
-import java.util.regex.Pattern
-
-
-fun isValidEmail(target: CharSequence?): Boolean {
-    return if (target == null) {
-        false
-    } else {
-        Patterns.EMAIL_ADDRESS.matcher(target).matches()
-    }
-}
-
-fun isValidLogin(login: CharSequence?): Boolean {
-    return if (login == null) {
-        false
-    } else {
-        val loginPattern: String = "^([0-9]*)(?=.*[a-z])([A-Z]*)(?=\\S+$).{4,}$"
-        val pattern: Pattern = Pattern.compile(loginPattern)
-        val matcher: Matcher = pattern.matcher(login)
-        matcher.matches()
-    }
-}
-
-fun writeErrorAboutLogin(login: CharSequence, activity: Activity) {
-    val loginPatternWithoutLCaseLetter: String = "^([0-9]*)([A-Z]*)(?=\\S+$).{4,}$"
-    var pattern: Pattern = Pattern.compile(loginPatternWithoutLCaseLetter)
-    var matcher: Matcher = pattern.matcher(login)
-    if (matcher.matches()) {
-        Toast.makeText(activity, "Login must contain at least one lower case letter", Toast.LENGTH_LONG).show()
-        return
-    }
-    val loginPatternWithSpace: String = "^([0-9]*)(?=.*[a-z])([A-Z]*).{4,}$"
-    pattern = Pattern.compile(loginPatternWithSpace)
-    matcher = pattern.matcher(login)
-    if (matcher.matches()) {
-        Toast.makeText(activity, "Login must not contain spaces", Toast.LENGTH_LONG).show()
-        return
-    }
-    val loginPatternShortLen: String = "^([0-9]*)(?=.*[a-z])([A-Z]*).{0,3}$"
-    pattern = Pattern.compile(loginPatternShortLen)
-    matcher = pattern.matcher(login)
-    if (matcher.matches()) {
-        Toast.makeText(activity, "Login must be at least 4 in length", Toast.LENGTH_LONG).show()
-        return
-    }
-
-    Toast.makeText(activity, "Incorrect login", Toast.LENGTH_LONG).show()
-}
-
-fun isValidPassword(password: CharSequence?): Boolean {
-    return if (password == null) {
-        false
-    } else {
-        val passwordPattern: String = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([@#$%^&+=]*)(?=\\S+$).{4,}$"
-        val pattern: Pattern = Pattern.compile(passwordPattern)
-        val matcher: Matcher = pattern.matcher(password)
-        matcher.matches()
-    }
-}
-
-fun writeErrorAboutPassword(password: CharSequence, activity: Activity) {
-    val passwordPatternWithoutDigit: String = "^(?=.*[a-z])(?=.*[A-Z])([@#$%^&+=]*)(?=\\S+$).{4,}$"
-    var pattern: Pattern = Pattern.compile(passwordPatternWithoutDigit)
-    var matcher: Matcher = pattern.matcher(password)
-    if (matcher.matches()) {
-        Toast.makeText(activity, "Password must contain at least one digit", Toast.LENGTH_LONG).show()
-        return
-    }
-    val passwordPatternWithoutLCaseLetter: String = "^(?=.*[0-9])(?=.*[A-Z])([@#$%^&+=]*)(?=\\S+$).{4,}$"
-    pattern = Pattern.compile(passwordPatternWithoutLCaseLetter)
-    matcher = pattern.matcher(password)
-    if (matcher.matches()) {
-        Toast.makeText(activity, "Password must contain at least one lower case letter", Toast.LENGTH_LONG).show()
-        return
-    }
-    val passwordPatternWithoutUCaseLetter: String = "^(?=.*[0-9])(?=.*[a-z])([@#$%^&+=]*)(?=\\S+$).{4,}$"
-    pattern = Pattern.compile(passwordPatternWithoutUCaseLetter)
-    matcher = pattern.matcher(password)
-    if (matcher.matches()) {
-        Toast.makeText(activity, "Password must contain at least one upper case letter", Toast.LENGTH_LONG).show()
-        return
-    }
-    val passwordPatternWithSpace: String = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([@#$%^&+=]*).{4,}$"
-    pattern = Pattern.compile(passwordPatternWithSpace)
-    matcher = pattern.matcher(password)
-    if (matcher.matches()) {
-        Toast.makeText(activity, "Password must not contain spaces", Toast.LENGTH_LONG).show()
-        return
-    }
-    val passwordPatternShortLen: String = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([@#$%^&+=]*)(?=\\S+\$).{0,3}$"
-    pattern = Pattern.compile(passwordPatternShortLen)
-    matcher = pattern.matcher(password)
-    if (matcher.matches()) {
-        Toast.makeText(activity, "Password must be at least 4 in length", Toast.LENGTH_LONG).show()
-        return
-    }
-
-    Toast.makeText(activity, "Incorrect password", Toast.LENGTH_LONG).show()
-}
-
-fun Fragment.hideKeyboard() {
-    view?.let { activity?.hideKeyboard(it) }
-}
-
-fun Activity.hideKeyboard() {
-    hideKeyboard(currentFocus ?: View(this))
-}
-
-fun Context.hideKeyboard(view: View) {
-    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-}
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -148,8 +23,8 @@ class SignUpActivity : AppCompatActivity() {
 
         val okHttpClient = OkHttpClient()
 
-        var loginEditText: TextInputEditText = findViewById(R.id.login)
-        var loginLayout: TextInputLayout = findViewById(R.id.login_box)
+        val loginEditText: TextInputEditText = findViewById(R.id.login)
+        val loginLayout: TextInputLayout = findViewById(R.id.login_box)
         loginEditText.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
                 hideKeyboard(v)
@@ -164,7 +39,7 @@ class SignUpActivity : AppCompatActivity() {
                 charSequence: CharSequence, start: Int, before: Int, count: Int
             ) {
                 val newText = charSequence.toString()
-                if (!isValidLogin(newText)) {
+                if (isNotValidLogin(newText)) {
                     loginLayout.error = "Unacceptable symbols in login"
                 } else {
                     // TODO: get login from db and check
@@ -179,8 +54,8 @@ class SignUpActivity : AppCompatActivity() {
         }
         loginEditText.addTextChangedListener(textWatcherForLogin)
 
-        var emailEditText: TextInputEditText = findViewById(R.id.email)
-        var emailLayout: TextInputLayout = findViewById(R.id.email_box)
+        val emailEditText: TextInputEditText = findViewById(R.id.email)
+        val emailLayout: TextInputLayout = findViewById(R.id.email_box)
         emailEditText.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
                 hideKeyboard(v)
@@ -193,7 +68,7 @@ class SignUpActivity : AppCompatActivity() {
 
             override fun onTextChanged(charSequence: CharSequence, start: Int, before: Int, count: Int) {
                 val newText = charSequence.toString()
-                if (!isValidEmail(newText)) {
+                if (isNotValidEmail(newText)) {
                     emailLayout.error = "This is not a valid email"
                 } else {
                     emailLayout.boxStrokeColor =
@@ -207,16 +82,16 @@ class SignUpActivity : AppCompatActivity() {
         }
         emailEditText.addTextChangedListener(textWatcherForEmail)
 
-        var passwordEditText: TextInputEditText = findViewById(R.id.password)
-        var passwordLayout: TextInputLayout = findViewById(R.id.password_box)
+        val passwordEditText: TextInputEditText = findViewById(R.id.password)
+        val passwordLayout: TextInputLayout = findViewById(R.id.password_box)
         passwordEditText.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
                 hideKeyboard(v)
             }
         }
 
-        var passwordConfirmEditText: TextInputEditText = findViewById(R.id.password_confirm)
-        var passwordConfirmLayout: TextInputLayout = findViewById(R.id.password_confirm_box)
+        val passwordConfirmEditText: TextInputEditText = findViewById(R.id.password_confirm)
+        val passwordConfirmLayout: TextInputLayout = findViewById(R.id.password_confirm_box)
         passwordConfirmEditText.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
                 hideKeyboard(v)
@@ -224,7 +99,7 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         fun checkPasswords(passwordOriginal: CharSequence, passwordConfirm: CharSequence) {
-            if (!isValidPassword(passwordOriginal)) {
+            if (isNotValidPassword(passwordOriginal)) {
                 passwordConfirmLayout.error = "Incorrect password"
                 passwordLayout.error = "Incorrect password"
             } else if (passwordOriginal != passwordConfirm) {
@@ -272,7 +147,7 @@ class SignUpActivity : AppCompatActivity() {
         }
         passwordConfirmEditText.addTextChangedListener(textWatcherForPasswordConfirm)
 
-        var signUpButton: Button = findViewById(R.id.sign_up_button)
+        val signUpButton: Button = findViewById(R.id.sign_up_button)
 
         signUpButton.setOnClickListener {
             val email: String = emailEditText.text.toString()
@@ -280,19 +155,19 @@ class SignUpActivity : AppCompatActivity() {
             val password: String = passwordEditText.text.toString()
             val passwordConfirm: String = passwordConfirmEditText.text.toString()
 
-            if (!isValidEmail(email)) {
+            if (isNotValidEmail(email)) {
                 Toast.makeText(this, "This is not an email", Toast.LENGTH_LONG).show()
                 Log.e("Sign up", "wrong email: $email")
                 return@setOnClickListener
             }
 
-            if (!isValidLogin(login)) {
+            if (isNotValidLogin(login)) {
                 writeErrorAboutLogin(login, this)
                 Log.e("Sign up", "incorrect login: $login")
                 return@setOnClickListener
             }
 
-            if (!isValidPassword(password)) {
+            if (isNotValidPassword(password)) {
                 writeErrorAboutPassword(password, this)
                 Log.e("Sign up", "incorrect password: $password")
                 return@setOnClickListener
@@ -304,109 +179,12 @@ class SignUpActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            val URlRegistration: String = ("http://" + getString(R.string.IP) + "/users/registration").toHttpUrlOrNull()
-                ?.newBuilder()
-                ?.build()
-                .toString()
+            tryToRegisterUser(login, email, password, this@SignUpActivity, okHttpClient, loginLayout, emailLayout)
 
-            val user: UserModel =
-                UserModel.newBuilder().setLogin(login).setEmail(email).setPasswordHash(password).build()
-            val requestBody: RequestBody =
-                RequestBody.create("application/x-protobuf".toMediaTypeOrNull(), user.toByteArray());
-
-            val requestForRegisterUser: Request = Request.Builder()
-                .url(URlRegistration)
-                .post(requestBody)
-                .build()
-
-            Log.i("Info", "Request has been sent $URlRegistration")
-            val somethingWentWrongMessage: String = "Something went wrong, try again"
-            okHttpClient.newCall(requestForRegisterUser).enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    Log.e("Error", e.toString() + " " + e.message)
-                    Handler(Looper.getMainLooper()).post {
-                        Toast.makeText(this@SignUpActivity, somethingWentWrongMessage, Toast.LENGTH_SHORT).show()
-                    }
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    Log.i("Info", response.toString())
-                    if (response.isSuccessful) {
-                        val URlGetUser: String =
-                            ("http://" + getString(R.string.IP) + "/users/userByLogin").toHttpUrlOrNull()
-                                ?.newBuilder()
-                                ?.addQueryParameter("login", login)
-                                ?.build().toString()
-
-                        val requestForGetGeneratedUser: Request = Request.Builder()
-                            .url(URlGetUser)
-                            .build()
-
-                        okHttpClient.newCall(requestForGetGeneratedUser).enqueue(object : Callback {
-                            override fun onFailure(call: Call, e: IOException) {
-                                Log.e("Error", e.toString() + " " + e.message)
-                                Handler(Looper.getMainLooper()).post {
-                                    Toast.makeText(
-                                        this@SignUpActivity,
-                                        somethingWentWrongMessage + " with another login",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
-                            }
-
-                            override fun onResponse(call: Call, response: Response) {
-                                Log.i("Info", response.toString())
-                                if (response.code == 200) {
-                                    val responseBody: ByteString? = response.body?.byteString()
-                                    val registredUser: UserModel = UserModel.parseFrom(responseBody?.toByteArray())
-                                    Handler(Looper.getMainLooper()).post {
-                                        Toast.makeText(
-                                            this@SignUpActivity,
-                                            "User has been successfully created",
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-                                    }
-                                    // TODO: create main Activity
-                                }
-                            }
-                        })
-
-                    } else {
-                        val loginErrorMessage: String = "user with that login already exist"
-                        val emailErrorMessage: String = "user with that email already exist"
-                        if (response.body?.string() == loginErrorMessage) {
-                            Handler(Looper.getMainLooper()).post {
-                                Toast.makeText(
-                                    this@SignUpActivity,
-                                    loginErrorMessage,
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                loginLayout.error = loginErrorMessage
-                            }
-                        } else if (response.body?.string() == emailErrorMessage) {
-                            Handler(Looper.getMainLooper()).post {
-                                Toast.makeText(
-                                    this@SignUpActivity,
-                                    emailErrorMessage,
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                emailLayout.error = emailErrorMessage
-                            }
-                        } else {
-                            Handler(Looper.getMainLooper()).post {
-                                Toast.makeText(
-                                    this@SignUpActivity,
-                                    somethingWentWrongMessage,
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }
-                    }
-                }
-            })
         }
 
-        var haveAnAccountButton: Button = findViewById(R.id.have_an_account)
+
+        val haveAnAccountButton: Button = findViewById(R.id.have_an_account)
         haveAnAccountButton.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
