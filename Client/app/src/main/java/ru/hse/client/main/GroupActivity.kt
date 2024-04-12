1475c9898a3d57938296ea2cede828daac11ca2d
romanpovol <roman.a.p@bk.ru>
2024-04-12 08:08:48 +0300

    add some features

---
+++ Client/app/src/main/java/ru/hse/client/main/GroupActivity.kt
@@ -0,0 +1,31 @@
+package ru.hse.client.main
+
+import android.os.Bundle
+import android.util.Log
+import ru.hse.client.databinding.ActivityGroupBinding
+import ru.hse.client.databinding.ActivityGroupSelectMenuBinding
+import ru.hse.server.proto.EntitiesProto
+
+class GroupActivity: DrawerBaseActivity() {
+
+    lateinit var binding: ActivityGroupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGroupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val bundle = intent.extras
+        var group: EntitiesProto.GroupModel? = null
+
+        if (bundle != null) {
+            group = bundle.getSerializable("group") as EntitiesProto.GroupModel
+        }
+
+        if (group != null) {
+            Log.i("INFO", group.name)
+        } else {
+            Log.e("ERROR", "ERROR")
+        }
+    }
+
+}
