ced4d7a51d81858ea17db7e0ab1b30968342d80b
romanpovol <roman.a.p@bk.ru>
2024-05-30 04:16:16 +0300

    add mark up

--- Client/app/src/main/java/ru/hse/client/chapters/ChapterUploadActivity.kt
+++ Client/app/src/main/java/ru/hse/client/chapters/ChapterUploadActivity.kt
@@ -1,6 +1,7 @@
 package ru.hse.client.chapters
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -8,6 +9,7 @@ import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import ru.hse.client.databinding.ActivityUploadChapterBinding
 import ru.hse.client.utility.DrawerBaseActivity
+import ru.hse.server.proto.EntitiesProto
 import java.io.*
 
 
@@ -15,6 +17,7 @@ class ChapterUploadActivity : DrawerBaseActivity() {
 
     private lateinit var binding: ActivityUploadChapterBinding
     private lateinit var filepath: Uri
+    private lateinit var chapter: EntitiesProto.ChapterModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +25,40 @@ class ChapterUploadActivity : DrawerBaseActivity() {
         setContentView(binding.root)
 
         binding.chooseFileButton.setOnClickListener {
-            Log.d("chooser", " 0")
+            Log.d("chooser", "0")
             startFileChooser()
         }
 
+        binding.helpButton.setOnClickListener {
+            Log.d("help", "0")
+            showHelpInfo()
+        }
 
+        binding.addTest.setOnClickListener {
+            Log.d("add test", "0")
+            addTest()
+        }
+
+    }
+
+    private fun addTest() {
+        TODO("Not yet implemented")
+    }
+
+    private fun showHelpInfo() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder
+            .setTitle("How to upload a file and create tests")
+            .setMessage(
+                "Select a file using the \"Choose\" button. Next, mark up the text. " +
+                        "Place the cursor in the place in the text after which the next test should begin and " +
+                        "click on the “+” button. When you finish uploading, click the \"Upload\" button.\n"
+            )
+            .setNegativeButton("I get it!", null)
+
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 
     private fun startFileChooser() {
@@ -35,7 +67,7 @@ class ChapterUploadActivity : DrawerBaseActivity() {
         startActivityForResult(Intent.createChooser(intent, "Choose file"), 111)
     }
 
-        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 111 && resultCode == Activity.RESULT_OK && data != null) {
             filepath = data.data!!
