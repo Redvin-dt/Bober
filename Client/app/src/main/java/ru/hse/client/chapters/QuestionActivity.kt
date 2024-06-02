d2a7630f9d1b15d40654d8b055c43b4744412572
romanpovol <roman.a.p@bk.ru>
2024-06-02 23:56:57 +0300

    add logic for quesionЭ

--- Client/app/src/main/java/ru/hse/client/chapters/QuestionActivity.kt
+++ Client/app/src/main/java/ru/hse/client/chapters/QuestionActivity.kt
@@ -1,6 +1,8 @@
 package ru.hse.client.chapters
 
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -9,17 +11,25 @@ import android.widget.Spinner
 import ru.hse.client.R
 import ru.hse.client.databinding.QuestionActivityBinding
 import ru.hse.client.utility.DrawerBaseActivity
+import java.util.LinkedList
 
-class QuestionActivity: DrawerBaseActivity() {
+class QuestionActivity : DrawerBaseActivity(), AnswerAdapter.OnCheckBoxListener {
     private lateinit var binding: QuestionActivityBinding
-    private var position: Int = 0
+    private var numberOfAnswers: Int = 0
+    private var correctAnswerNumbers: LinkedList<Int> = LinkedList()
+
+    private enum class QuestionType {
+        SINGLE, MULTIPLE
+    }
+
+    private var questionType: QuestionType = QuestionType.SINGLE
     private lateinit var listOfAnswers: ListView
-    private lateinit var answerData: AnswerData
     private lateinit var answerAdapter: AnswerAdapter
-    private var dataArrayList: ArrayList<AnswerData> = ArrayList()
-    private val listOfAnswersHints = listOf("Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5")
+    private var dataArrayList: MutableList<AnswerData> = ArrayList()
+    private val listOfAnswersHints =
+        listOf("Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5")
     private val listOfAnswersNumbers = listOf("1", "2", "3", "4", "5")
-    private val listOfTypes = listOf("multiple", "one")
+    private val listOfTypes = listOf("multiple", "single")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,18 +43,36 @@ class QuestionActivity: DrawerBaseActivity() {
     }
 
     private fun initAnswersList() {
-        position = 0
+        numberOfAnswers = 1
         listOfAnswers = binding.listView
-        for (number in listOfAnswersHints) {
-            answerData = AnswerData(number, false)
-            dataArrayList.add(answerData)
-        }
+        val answerData =
+            AnswerData(listOfAnswersHints[0], false, Editable.Factory.getInstance().newEditable(""))
+        dataArrayList.add(answerData)
 
         answerAdapter = AnswerAdapter(this, dataArrayList)
         binding.listView.adapter = answerAdapter
         binding.listView.isClickable = true
-
+        answerAdapter.setOnCheckboxCheckedListener(this)
     }
+
+    override fun onCheckBoxChecked(position: Int, isChecked: Boolean) {
+        if (!isChecked) {
+            correctAnswerNumbers.remove(position)
+            return
+        }
+        if (questionType == QuestionType.SINGLE) {
+            while (correctAnswerNumbers.size > 0) {
+                val positionOfRemovedCheckBox = correctAnswerNumbers.first
+                correctAnswerNumbers.removeFirst()
+                answerAdapter.updateCheckboxState(positionOfRemovedCheckBox, false)
+            }
+            answerAdapter.updateCheckboxState(position, true)
+            correctAnswerNumbers.addLast(position)
+        } else {
+            correctAnswerNumbers.addLast(position)
+        }
+    }
+
 
     private fun initSpinners() {
         val spinnerNumberOfTests: Spinner = binding.spinnerNumber
@@ -57,15 +85,38 @@ class QuestionActivity: DrawerBaseActivity() {
             spinnerNumberOfTests.adapter = adapter
         }
 
-        spinnerNumberOfTests.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        spinnerNumberOfTests.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
                 val selectedItem = parent?.getItemAtPosition(position).toString()
-                TODO("Not yet implemented")
+                Log.d("spinnerNumberOfTests", "selected item: $selectedItem")
+                val currentNumberOfAnswers = id + 1
+                if (currentNumberOfAnswers < numberOfAnswers) {
+                    while (currentNumberOfAnswers.toInt() != dataArrayList.size) {
+                        answerAdapter.removeItem(dataArrayList.size - 1)
+                    }
+                    binding.listView.adapter = answerAdapter
+                    numberOfAnswers = currentNumberOfAnswers.toInt()
+                } else {
+                    while (currentNumberOfAnswers > numberOfAnswers) {
+                        val answerData = AnswerData(
+                            listOfAnswersHints[numberOfAnswers],
+                            false,
+                            Editable.Factory.getInstance().newEditable("")
+                        )
+                        dataArrayList.add(answerData)
+                        numberOfAnswers++
+                    }
+                    binding.listView.adapter = answerAdapter
+                    numberOfAnswers = currentNumberOfAnswers.toInt()
+                }
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
 
         }
 
@@ -79,15 +130,28 @@ class QuestionActivity: DrawerBaseActivity() {
             spinnerTestType.adapter = adapter
         }
 
-        spinnerTestType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        spinnerTestType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
                 val selectedItem = parent?.getItemAtPosition(position).toString()
-                TODO("Not yet implemented")
+                Log.d("spinnerTestType", "selected item: $selectedItem")
+                if (selectedItem == "multiple") {
+                    questionType = QuestionType.MULTIPLE
+                } else {
+                    questionType = QuestionType.SINGLE
+                    while (correctAnswerNumbers.size > 1) {
+                        val positionOfRemovedCheckBox = correctAnswerNumbers.first
+                        correctAnswerNumbers.removeFirst()
+                        answerAdapter.updateCheckboxState(positionOfRemovedCheckBox, false)
+                    }
+                }
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
 
         }
     }
