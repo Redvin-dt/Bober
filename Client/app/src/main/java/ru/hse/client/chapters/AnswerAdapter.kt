48113cf897598d6b92cbf66b4b9406ec0c4a97b5
romanpovol <roman.a.p@bk.ru>
2024-06-02 23:56:57 +0300

    add logic for quesionЭ

--- Client/app/src/main/java/ru/hse/client/chapters/AnswerAdapter.kt
+++ Client/app/src/main/java/ru/hse/client/chapters/AnswerAdapter.kt
@@ -1,40 +1,92 @@
 package ru.hse.client.chapters
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.ArrayAdapter
 import android.widget.CheckBox
+import com.google.android.material.textfield.TextInputEditText
 import ru.hse.client.R
 
-
-class AnswerAdapter(context: Context, dataArrayList: ArrayList<AnswerData>) :
+class AnswerAdapter(context: Context, private val dataArrayList: MutableList<AnswerData>) :
     ArrayAdapter<AnswerData>(context, R.layout.answer_list_item, dataArrayList) {
 
     private val itemsPadding = 40F
+    private val enteredTextMap = mutableMapOf<Int, String>()
+    private val conditionMap = mutableMapOf<Int, Boolean>()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = convertView ?: LayoutInflater.from(context)
+            .inflate(R.layout.answer_list_item, parent, false)
 
-        var view = convertView
-        val listAnswerData = getItem(position)
+        val textWidget = view.findViewById<TextInputEditText>(R.id.answer)
+        val checkBox = view.findViewById<CheckBox>(R.id.checkBox)
 
-        if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.answer_list_item, parent, false)
+        val listAnswerData = dataArrayList[position]
+        textWidget.hint = listAnswerData.hint
+
+        if (conditionMap.containsKey(position)) {
+            conditionMap[position]?.let { checkBox.setChecked(it) }
+        } else {
+            checkBox.isChecked = listAnswerData.isCorrect
         }
-        val answer =
-            view!!.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.answer)
-        val isCorrect = view.findViewById<CheckBox>(R.id.checkBox)
-        if (listAnswerData != null) {
-            answer.hint = listAnswerData.answer
-            isCorrect.isChecked = listAnswerData.isCorrect
+
+        if (enteredTextMap.containsKey(position)) {
+            textWidget.setText(enteredTextMap[position])
+        } else {
+            textWidget.text = listAnswerData.answer
         }
+
+        checkBox.setOnCheckedChangeListener { _, isChecked ->
+            conditionMap[position] = isChecked
+            onCheckBoxCheckedListener?.onCheckBoxChecked(position, isChecked)
+        }
+
+        textWidget.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                enteredTextMap[position] = s.toString()
+            }
+        })
 
         view.translationY = itemsPadding
 
         return view
     }
 
+    override fun getItem(position: Int): AnswerData {
+        return dataArrayList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    fun removeItem(position: Int) {
+        dataArrayList.removeAt(position)
+        enteredTextMap.remove(position)
+        conditionMap.remove(position)
+        notifyDataSetChanged()
+    }
+
+    fun updateCheckboxState(position: Int, isChecked: Boolean) {
+        dataArrayList[position].isCorrect = isChecked
+        conditionMap[position] = isChecked
+        notifyDataSetChanged()
+    }
+
+    interface OnCheckBoxListener {
+        fun onCheckBoxChecked(position: Int, isChecked: Boolean)
+    }
+
+    private var onCheckBoxCheckedListener: OnCheckBoxListener? = null
+    fun setOnCheckboxCheckedListener(listener: OnCheckBoxListener) {
+        onCheckBoxCheckedListener = listener
+    }
 }
