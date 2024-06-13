55850875cb3e822193b904959e7ec2e72862a979
YuriiFilatov <yura_filatov_2004@mail.ru>
2024-06-13 19:58:09 +0300

    Rebased

--- Client/app/src/main/java/ru/hse/client/chapters/AnswerAdapter.kt
+++ Client/app/src/main/java/ru/hse/client/chapters/AnswerAdapter.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
